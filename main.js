a0268a1e62a32f4c113e8cd44f853646d72b1b3d
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-03-08 13:36:41 -0500

    .attr vs .getAttribute

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const $links = $("a"); // Links to all <a> selectors which contain the class 'da
 $("input").on("keyup", () => {
   const $inputVal = $input.val();
   for (let i = 0; i < $links.length; i += 1) {
-    var $caption = $links[i].attr("data-title");
+    var $caption = $links[i].getAttribute("data-title");
     if ($caption.toUpperCase().indexOf($inputVal.toLowerCase()) != -1) {
       $links[i].style.display = "block";
     } else {
