3d5368cb31a3760bd951107e5667debb4ded3884
Lukas Vukovich <lukas.vukovich@gmail.com>
2019-03-08 14:01:17 -0500

    filter fixed - need to remove li so formatting is good after search

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-const $input = $("input"); // Input field
 const $links = $("a"); // Links to all <a> selectors which contain the class 'data-title'
 
-//PROGRAM
-
-$("input").on("keyup", () => {
-  const $inputVal = $input.val();
+$("input").on("keyup", e => {
+  let val = e.target.value;
   for (let i = 0; i < $links.length; i += 1) {
     var $caption = $links[i].getAttribute("data-title");
-    if ($caption.toUpperCase().indexOf($inputVal.toLowerCase()) != -1) {
+    if ($caption.toUpperCase().indexOf(val.toUpperCase()) != -1) {
       $links[i].style.display = "block";
     } else {
       $links[i].style.display = "none";
